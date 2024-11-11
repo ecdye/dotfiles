17abb595b910d980cf3f955eb4ead979d9dde3c5
Ethan Dye <mrtops03@gmail.com>
2024-11-11 10:21:10 -0700

    Use Kickstart
    Signed-off-by: Ethan Dye <mrtops03@gmail.com>

--- nvim/lua/plugins/fugitive.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "tpope/vim-fugitive",
-    config = function()
-        vim.opt.statusline = '  %< %{FugitiveHead()}  %f %m %r %w %= Ln %l, Col %c  %{&fileencoding?&fileencoding:&encoding}  '
-    end,
-
-}
--- nvim/lua/plugins/gitgutter.lua
+++
@@ -1,6 +0,0 @@
-return {
-    "airblade/vim-gitgutter",
-    config = function()
-        vim.opt.updatetime = 100 -- Update every 100ms
-    end,
-}
